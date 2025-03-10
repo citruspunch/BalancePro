20aab56db45b6619c936bfeacb914b586acd08b4
andrestobar05 <andrewtobar05@gmail.com>
2025-03-09 22:45:52 -0600

    Add database configuration file for PDO connection

---
+++ src/config/database.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost'; 
+$dbname = 'CONTABILIDAD'; 
+$username = 'root';
+$password = '';
+
+try {
+    
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
