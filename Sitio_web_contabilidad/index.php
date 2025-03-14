0d35f52b6c61acc6505f798217370e6ec011efd1
andrestobar05 <andrewtobar05@gmail.com>
2025-03-14 13:23:08 -0600

    Add index.php for routing and controller handling in the application

---
+++ Sitio_web_contabilidad/index.php
@@ -0,0 +1,56 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require_once './src/config/database.php';
+require_once './src/controllers/CuentasController.php';
+require_once './src/controllers/PolizasController.php';
+require_once './src/controllers/DetallePolizaController.php';
+
+$cuentasController = new CuentasController($conn);
+$polizasController = new PolizasController($conn);
+$detallePolizaController = new DetallePolizaController($conn);
+
+// Map de Rutas a controladores
+$routes = [
+    '/' => './src/views/layout.php',
+    '/Sitio_web_contabilidad' => './src/views/layout.php',
+    '/Sitio_web_contabilidad/cuentas' => [$cuentasController, 'handleRequest'],
+    '/Sitio_web_contabilidad/cuentas/add' => [$cuentasController, 'addCuenta'],
+    '/Sitio_web_contabilidad/cuentas/edit' => [$cuentasController, 'editCuenta'],
+    '/Sitio_web_contabilidad/cuentas/delete' => [$cuentasController, 'deleteCuenta'],
+    '/Sitio_web_contabilidad/polizas' => [$polizasController, 'listPolizas'],
+    '/Sitio_web_contabilidad/polizas/add' => [$polizasController, 'addPoliza'],
+    '/Sitio_web_contabilidad/polizas/edit' => [$polizasController, 'editPoliza'],
+    '/Sitio_web_contabilidad/polizas/delete' => [$polizasController, 'deletePoliza'],
+    '/Sitio_web_contabilidad/detallePoliza' => [$detallePolizaController, 'list'],
+    '/Sitio_web_contabilidad/detallePoliza/add' => [$detallePolizaController, 'add'],
+    '/Sitio_web_contabilidad/detallePoliza/edit' => [$detallePolizaController, 'edit'],
+    '/Sitio_web_contabilidad/detallePoliza/delete' => [$detallePolizaController, 'delete'],
+    '/Sitio_web_contabilidad/reportes' => './src/views/reportes/' . basename($_SERVER['REQUEST_URI']) . '.php',
+];
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$requestUri = strtok($requestUri, '?'); 
+$requestUri = rtrim($requestUri, '/'); 
+
+if (array_key_exists($requestUri, $routes)) {
+    $handler = $routes[$requestUri];
+    if (is_callable($handler)) {
+        call_user_func($handler);
+    } elseif (is_string($handler)) {
+        include $handler;
+    }
+} else {
+    // Debug de la ruta Imprimir la ruta y el método
+    echo $requestUri . ' ' . $requestMethod;
+    http_response_code(404);
+    echo "404 Not Founddddd";
+}
+?>
