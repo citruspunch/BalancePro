238dca513cbbb2723f2352f0055669195b3f1094
andrestobar05 <andrewtobar05@gmail.com>
2025-03-15 12:19:50 -0600

    Remove versioning from CSS stylesheet link in layout.php

--- Sitio_web_contabilidad/src/views/layout.php
+++ Sitio_web_contabilidad/src/views/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contabilidad App</title>
-    <link rel="stylesheet" href="/Sitio_web_contabilidad/public/css/styles.css?v=1.0">
+    <link rel="stylesheet" href="/Sitio_web_contabilidad/public/css/styles.css">
 </head>
 <body>
     <div class="wrapper">
