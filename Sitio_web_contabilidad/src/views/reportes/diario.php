07590e1be485075253003b95609553c60188bb10
andrestobar05 <andrewtobar05@gmail.com>
2025-03-18 10:12:34 -0600

    Refactor diario.php to enhance the display of polizas and their details, including total calculations and improved formatting.

--- Sitio_web_contabilidad/src/views/reportes/diario.php
+++ Sitio_web_contabilidad/src/views/reportes/diario.php
@@ -27,51 +27,49 @@
             </form>
 
             <?php if (!empty($polizas)): ?>
-                <h3>Pólizas</h3>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Número de Póliza</th>
-                            <th>Fecha</th>
-                            <th>Descripción</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($polizas as $poliza): ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($poliza['NumPoliza']); ?></td>
-                                <td><?php echo htmlspecialchars($poliza['Fecha']); ?></td>
-                                <td><?php echo htmlspecialchars($poliza['Descripcion']); ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            <?php else: ?>
-                <p>No se encontraron polizas para la fecha especificada.</p>
-            <?php endif; ?>
+                <br> <br>
+                <?php foreach ($polizas as $poliza): ?>
+                    <p><strong>Póliza Número: <?php echo htmlspecialchars($poliza['NumPoliza']); ?></strong></p>
+                    <p><strong>Fecha:</strong> <?php echo htmlspecialchars($poliza['Fecha']); ?></p>
+                    <p><strong>Descripción:</strong> <?php echo htmlspecialchars($poliza['Descripcion']); ?></p>
 
-            <?php if (!empty($detalles)): ?>
-                <h3>Detalles de las Pólizas</h3>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Número de Póliza</th>
-                            <th>Nombre de Cuenta</th>
-                            <th>Debe</th>
-                            <th>Haber</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($detalles as $detalle): ?>
+                    <table class="table table-bordered">
+                        <thead>
                             <tr>
-                                <td><?php echo htmlspecialchars($detalle['NumPoliza']); ?></td>
-                                <td><?php echo htmlspecialchars($detalle['NombreCuenta']); ?></td>
-                                <td><?php echo $detalle['DebeHaber'] === 'D' ? htmlspecialchars($detalle['Valor']) : ''; ?></td>
-                                <td><?php echo $detalle['DebeHaber'] === 'H' ? htmlspecialchars($detalle['Valor']) : ''; ?></td>
+                                <th>Nombre de Cuenta</th>
+                                <th>Debe</th>
+                                <th>Haber</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $totalDebe = 0;
+                            $totalHaber = 0;
+                            foreach ($detalles as $detalle):
+                                if ($detalle['NumPoliza'] === $poliza['NumPoliza']):
+                                    $totalDebe += $detalle['DebeHaber'] === 'D' ? $detalle['Valor'] : 0;
+                                    $totalHaber += $detalle['DebeHaber'] === 'H' ? $detalle['Valor'] : 0;
+                                    ?>
+                                    <tr>
+                                        <td><?php echo htmlspecialchars($detalle['NombreCuenta']); ?></td>
+                                        <td class="valor"><?php echo $detalle['DebeHaber'] === 'D' ? htmlspecialchars(number_format($detalle['Valor'], 2)) : ''; ?></td>
+                                        <td class="valor"><?php echo $detalle['DebeHaber'] === 'H' ? htmlspecialchars(number_format($detalle['Valor'],2)) : ''; ?></td>
+                                    </tr>
+                                <?php
+                                endif;
+                            endforeach;
+                            ?>
+                            <tr>
+                                <td><strong>Totales</strong></td>
+                                <td><strong><?php echo number_format($totalDebe, 2); ?></strong></td>
+                                <td><strong><?php echo number_format($totalHaber, 2); ?></strong></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p class="alert alert-warning">No se encontraron pólizas para la fecha especificada.</p>
             <?php endif; ?>
 
             <a href="/Sitio_web_contabilidad" class="btn btn-secondary">Volver</a>
